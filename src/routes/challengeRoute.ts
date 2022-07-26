2c0fb90efc8641f83ea1f67e8616b6d379508b35
PedroHercules <pedroherculesdantas@gmail.com>
2022-07-26 16:18:01 -0300

    added upload image to challenege

--- src/routes/challengeRoute.ts
+++ src/routes/challengeRoute.ts
@@ -3,9 +3,11 @@ import express from 'express';
 import { ChallengeController } from '../controllers/challengeController';
 import { authorize } from '../middleware/authMiddleware';
 
+import { uploads } from '../uploadMulter';
+
 export const routesChallenge = express.Router()
 
 const challengeController = new ChallengeController();
 
-routesChallenge.post('/', authorize, challengeController.create);
+routesChallenge.post('/', authorize, uploads.single('image'), challengeController.create);
 routesChallenge.get('/', challengeController.index)
