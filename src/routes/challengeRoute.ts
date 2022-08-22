ae7e42d2649735ce08340461537d97bbe9b9e681
PedroHercules <pedroherculesdantas@gmail.com>
2022-08-22 13:47:06 -0300

    Create another multer to send image solutions

--- src/routes/challengeRoute.ts
+++ src/routes/challengeRoute.ts
@@ -3,10 +3,10 @@ import express from 'express';
 import { challengeController } from '../controllers/challengeController';
 import { authorize } from '../middleware/authMiddleware';
 
-import { uploads } from '../uploadMulter';
+import { uploadsChallenge } from '../uploadMulter';
 
 export const routesChallenge = express.Router()
 
-routesChallenge.post('/', authorize, uploads.single('image'), challengeController.create);
+routesChallenge.post('/', authorize, uploadsChallenge.single('image'), challengeController.create);
 routesChallenge.get('/', challengeController.index);
 routesChallenge.get('/:id', challengeController.get);
